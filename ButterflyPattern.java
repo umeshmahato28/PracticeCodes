4b589496e7b530f42cc1ec7c353746e132eb553e
Umesh Mahato <112267835+umeshmahato28@users.noreply.github.com>
2025-01-07 12:20:58 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -1,6 +1,6 @@
 public class ButterflyPattern {
       public static void main(String[] args) {
-            int n = 4;
+            int n = 4; 
 
             for(int i=1; i<=n; i++){
                   for(int j =1; j<=i; j++){
